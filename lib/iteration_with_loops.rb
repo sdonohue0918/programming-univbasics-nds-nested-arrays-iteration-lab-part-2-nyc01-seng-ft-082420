1ff482fc71fea3ff6a1be7140bedba2b7de0115c
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-08-02 22:33:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -22,12 +22,18 @@ def find_min_in_nested_arrays(src)
   # Produce a new Array that contains the smallest number of each of the nested arrays
 
   row_index = 0
-    while row_index < src.count do
-      element_index = 0
-      while element_index <src[row_index].count do
-        if {|a, b| a < b}
-      end
+    dailytemp_mins_array = []
+    while row_index<src.count do
+    
+     dailytemp_mins_array.push(src[row_index].min)
+     
+     row_index += 1 
+  
     end
+  
+  return dailytemp_mins_array
+  
+  end
 
 
 
